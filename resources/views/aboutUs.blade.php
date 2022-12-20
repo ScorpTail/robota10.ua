5783967e155e1dd8d7578da0b61f64f56807f22a
ScorpTail <scorpio232354@gmail.com>
2022-12-20 15:01:55 +0200

    Initial commit

---
+++ resources/views/aboutUs.blade.php
@@ -0,0 +1,15 @@
+<!doctype html>
+<html lang="uk">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>About Us</title>
+</head>
+<body>
+<h1><strong>HELLO WORLD ABOUT US</strong></h1>
+<a href="/">Main page</a><br>
+<a href="profile">Profile page</a>
+</body>
+</html>
---
+++ resources/views/mainPage.blade.php
@@ -0,0 +1,15 @@
+<!doctype html>
+<html lang="uk">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>About Us</title>
+</head>
+<body>
+<h1><strong>HELLO WORLD MAIN PAGE</strong></h1>
+<a href="profile">Profile page</a><br>
+<a href="aboutUs">Abouts Us page</a>
+</body>
+</html>
